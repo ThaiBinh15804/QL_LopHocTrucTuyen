1f5385dcd26dde038d906b60eae7dc3f4fd08d11
Vo Thai Binh <2001220401@hufi.edu.vn>
2024-12-11 09:27:43 +0700

    sua connect

--- QL_LopHocTrucTuyen/QL_LopHocTrucTuyen/App_Start/RouteConfig.cs
+++ QL_LopHocTrucTuyen/QL_LopHocTrucTuyen/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace QL_LopHocTrucTuyen
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "GiangVien", action = "DangNhap", id = UrlParameter.Optional }
+                defaults: new { controller = "HocVien", action = "DangNhap", id = UrlParameter.Optional }
             );
         }
     }
